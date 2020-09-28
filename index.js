4b2280a6c0659c02d81ea29c4ae66a01dc322dea
wkd3475 <wkd3475@gmail.com>
2020-09-28 23:59:31 +0900

    fix : element에 대한 체크가 이루어지기 전에 property 접근하는 오류 수정

--- index.js
+++ index.js
@@ -269,9 +269,8 @@ class JFMServer {
     decompress = (arr) => {
         let result = [];
         for (let element of arr) {
-            let formatId = element["i"];
-
             if (typeof(element["c"]) !== "undefined") {
+                let formatId = element["i"];
                 for (let d of element["c"]) {
                     result.push({"i": formatId, "d": d});
                 }
